97f747df3bc859485f520f0dbd3785c23ef94331
Yaroslav <yaroslav.belyanski@yandex.ru>
2023-10-01 22:13:52 +0300

    Added guestbook

---
+++ personal_website/guestbook/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.urls import reverse
+
+from .models import GuestBookEntry
+
+class GuestBookEntryForm(forms.ModelForm):
+    class Meta:
+        model = GuestBookEntry
+        fields = ['name', 'message']
+    
+    def get_success_url(self):
+        return reverse('guestbook')
+    
+    class Meta:
+        model = GuestBookEntry
+        fields = ['name', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-95', 'id': 'inputPassword3'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['message'].initial = ''
+
--- personal_website/guestbook/views.py
+++ personal_website/guestbook/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 
+from .models import GuestBookEntry
+from .forms import GuestBookEntryForm  # Создайте эту форму
 
 def guestbook(request):
-    return render(request, 'guestbook/guestbook.html')
+    entries = GuestBookEntry.objects.order_by('-timestamp')
+    paginator = Paginator(entries, 6)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    
+    return render(request, 'guestbook/guestbook.html', {'page': page})
 
 def guest_entries(request):
-    return render(request, 'guestbook/guest_entries.html')
+    if request.method == 'POST':
+        form = GuestBookEntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('guestbook:guestbook')
+    else:
+        form = GuestBookEntryForm()
+    
+    return render(request, 'guestbook/guest_entries.html', {'form': form})
