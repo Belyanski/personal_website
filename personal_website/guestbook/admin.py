fa0ca053a4a9165ae506b095d3dca06e208dbfb4
Yaroslav <yaroslav.belyanski@yandex.ru>
2023-10-08 10:29:59 +0300

    Register guestbook

--- personal_website/guestbook/admin.py
+++ personal_website/guestbook/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import GuestBookEntry
 
-# Register your models here.
+@admin.register(GuestBookEntry)
+class GuestBookEntryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'timestamp')
+    list_filter = ('timestamp',)
+    search_fields = ('name', 'message')
+    date_hierarchy = 'timestamp'
+
